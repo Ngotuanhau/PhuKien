a75434c67fa16af76164e7b54816cd0352969f09
tuanhau <ngotuanhaudn@gmail.com>
2019-08-30 15:08:52 +0700

    init commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "@mdi/font/css/materialdesignicons.css";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: "mdi"
+    }
+});

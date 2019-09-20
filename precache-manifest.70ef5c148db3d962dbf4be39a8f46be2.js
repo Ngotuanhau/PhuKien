8dd10a3c5138f2ac7f9196544040b38c30f9a2ed
tuanhau <ngotuanhaudn@gmail.com>
2019-09-20 14:39:10 +0700

    first commit

---
+++ precache-manifest.70ef5c148db3d962dbf4be39a8f46be2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b60a2303be6012019dc15bcb05701321",
+    "url": "/fonts/materialdesignicons-webfont.b60a2303.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "16dbed5533257a0e0d89",
+    "url": "/js/chunk-vendors.b3a6a7dc.js"
+  },
+  {
+    "revision": "7821a54b143f849a2db4",
+    "url": "/js/app.a7901fac.js"
+  },
+  {
+    "revision": "dc1d8150a2160f53cac52b5021681bf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "d89054b1a76534c07c6a59c5dd9094aa",
+    "url": "/fonts/materialdesignicons-webfont.d89054b1.woff2"
+  },
+  {
+    "revision": "53dc8616d656cfdeab5be0cff4c4a12b",
+    "url": "/fonts/materialdesignicons-webfont.53dc8616.ttf"
+  },
+  {
+    "revision": "443b30cdc6d4980c19e50226b0013cb6",
+    "url": "/fonts/materialdesignicons-webfont.443b30cd.eot"
+  },
+  {
+    "revision": "16dbed5533257a0e0d89",
+    "url": "/css/chunk-vendors.9cd9fab0.css"
+  },
+  {
+    "revision": "7821a54b143f849a2db4",
+    "url": "/css/app.56db3c19.css"
+  }
+];
